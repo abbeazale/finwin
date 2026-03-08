fff871691e035be6be03dfeb1c7eab1e18c48c77
Abbe Azale <abbeazale98@gmail.com>
2026-03-08 15:08:08 -0700

    implementing drizzle user schema and better-auth configs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,16 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/index";
 
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
+    database: drizzleAdapter(db,{
+        provider: "pg",
+        usePlural: true
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
     socialProviders: { 
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID as string, 
